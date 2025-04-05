54e227de43f46f03952219d7e93296b2bf215d5d
rohitc154 <rohitmonu154@gmail.com>
2025-04-05 18:45:30 +0530

    Merge remote and local repos

---
+++ JavaScript/hello.js
@@ -0,0 +1,13 @@
+console.log("Hello rohit!");
+
+// Cheking type of a variable
+
+let score = 89
+console.log(typeof score);
+
+if (score === 'number') {
+    console.log("Ye!, it's a number");
+}else{
+    console.log("Not number");
+    
+}
